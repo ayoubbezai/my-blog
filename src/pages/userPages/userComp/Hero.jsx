dd5b1bb9334d62b81ea379ac7892a057d630a113
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-20 04:24:05 +0100

    styling

--- src/pages/userPages/userComp/Hero.jsx
+++ src/pages/userPages/userComp/Hero.jsx
@@ -1,7 +1,7 @@
 
 const Hero = () => {
   return (
-    <div className="b text-white py-4 w-[90%] mx-auto my-10 border border-x-0 ">
+    <div className="b text-white py-4 w-[90%] mx-auto my-10 mt-20 border border-x-0 ">
       <h1 className="text-3xl md:text-8xl font-bold text-center my-8">Welcome to the blog</h1>
     </div>
   )

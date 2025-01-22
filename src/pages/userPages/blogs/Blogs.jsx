760c7fde437e713c243936c5b80322710e8af0a4
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-22 12:02:15 +0100

    styling

--- src/pages/userPages/blogs/Blogs.jsx
+++ src/pages/userPages/blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import NavBar from "../userComp/NavBar"
 import AllBlogs from "../userComp/AllBlogs"
 const Blogs = () => {
   return (
-    <div className="bg-primary  md:flex flex-col md:flex-row ">
+    <div className="bg-primary min-h-screen  md:flex flex-col md:flex-row ">
       <NavBar hoverd={2} />
       <AllBlogs />
     </div>

873878b28f981d602d7961e940e5cfffc46bc56e
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-31 19:49:38 +0100

    finsh user role

--- src/pages/userPages/userComp/OneBlog.jsx
+++ src/pages/userPages/userComp/OneBlog.jsx
@@ -6,7 +6,7 @@ const OneBlog = ({ blog }) => {
             <img
                 src={blog.imageUrl}
                 alt="Blog visual"
-                className="rounded-md lg:w-4/5 max-w-52 h-52 max-h-64 object-cover"
+                className="rounded-md lg:w-4/5 h-52 object-cover"
             />
             <p className="text-xs md:text-sm font-medium leading-5 text-white lg:w-4/5  mt-4 break-words">
                 {blog.bigDescription.substring(0, 200)}...

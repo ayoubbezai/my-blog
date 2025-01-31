06374db5b3ba9d47e35f699abd139fe4009325aa
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-02-01 00:06:49 +0100

    edit blog

--- src/pages/adminPages/adminComp/EditBlog.jsx
+++ src/pages/adminPages/adminComp/EditBlog.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../../../context/AuthContext";
 import toast, { Toaster } from 'react-hot-toast';
 import { createPortal } from 'react-dom';
 import AddBlog from "../../adminPages/adminComp/AddBlog";
-import { Link } from 'react-router-dom';
+import OneBlog from "@/pages/userPages/userComp/OneBlog";
 
 const Button = ({ onClick, children, className = "" }) => (
     <button
@@ -110,57 +110,31 @@ const EditBlog = () => {
                 document.body
             )}
 
-            {blogs.length === 0 ? (
-                <h1 className="text-2xl font-bold self-center">No blogs found</h1>
-            ) : (
-                <h1 className="text-2xl my-4 md:text-4xl font-bold text-center text-secondary">Remove Blogs</h1>
-            )}
+            {blogs.length === 0 ?
+                <h1 className="text-2xl font-bold self-center">No blogs found</h1> :
+                <h1 className="text-2xl mt-4 md:text-4xl font-bold text-center text-secondary">Remove Blogs</h1>
+            }
 
-            <div className="overflow-x-auto max-h-96  relative shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-800 max-h-96 o dark:bg-gray-800 dark:text-gray-400">
-                        <tr>
-                            <th className="px-6 py-3">Title</th>
-                            <th className="px-6 py-3">Author</th>
-                            <th className="px-6 py-3">Image</th>
-                            <th className="px-6 py-3">Description</th>
-                            <th className="px-6 py-3">Tags</th>
-                            <th className="px-6 py-3">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {displayedBlogs.map(blog => (
-                            <tr key={blog.id} className="bg-gray-800 border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td className="px-6 py-4 text-white">{blog.title}</td>
-                                <td className="px-6 py-4 text-white">{blog.createdBy?.name || "Unknown Author"}</td>
-                                <td className="px-6 py-4">
-                                    <img src={blog.imageUrl} alt="Blog visual" className="h-20 w-20 object-cover rounded-md" />
-                                </td>
-                                <td className="px-6 py-4 text-white">{blog.bigDescription.substring(0, 100)}...</td>
-                                <td className="px-6 py-4">
-                                    {blog.tags && blog.tags.map((tag, index) => (
-                                        <span key={index} className="inline-block m-1 px-2 py-1 text-xs bg-gradient-to-r from-secondary to-green-500 text-white rounded-md">{tag}</span>
-                                    ))}
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link to={`/blog/${blog.id}`} className="text-white font-semibold rounded-lg shadow-lg p-2 px-6 hover:opacity-90 transition bg-secondary my-2">More</Link>
-                                    <Button onClick={() => remove(blog.id)} className="bg-red-500 mt-2">Delete</Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-12 p-12 relative">
+                {displayedBlogs.map(blog => (
+                    <div key={blog.id} className="relative mx-auto bg-gray-800 p-4  rounded-md shadow-md flex flex-col gap-4">
+                        <OneBlog blog={blog} />
+                        <div className="flex justify-between">
+                            <Button onClick={() => remove(blog.id)} className="bg-red-500">Delete</Button>
+                        </div>
+                    </div>
+                ))}
             </div>
+            <Toaster />
 
             {/* Load More Button */}
             {lastVisible && (
-                <div className="flex justify-center mt-8">
-                    <Button onClick={fetchMoreBlogs} className="mb-16 bg-gradient-to-r from-secondary to-green-500">
+                <div className="flex justify-center">
+                    <Button onClick={fetchMoreBlogs} className="mb-16 bg-gradient-to-r from-secondary to-green-500 ">
                         {loading ? "Loading..." : "Load More"}
                     </Button>
                 </div>
             )}
-            <Toaster />
         </>
     );
 };
